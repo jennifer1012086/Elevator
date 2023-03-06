cf12fb2090322a4b0f1205bab3009ed6895a0353
jennifer Chen <63331121+jennifer1012086@users.noreply.github.com>
2023-03-06 23:08:21 +0800

    Add files via upload

--- Elevator_Simulator.cpp
+++ Elevator_Simulator.cpp
@@ -10,32 +10,44 @@ class Elevator
     public:
         int get_floor() { return current_floor; } // define a new method to get the current floor
         void set_floor(int new_floor) { current_floor = new_floor; } // define a new method to set the current floor
-        void display_floor(){ cout << current_floor << endl; } // display the current floor
+        void display_floor() // display the current floor
+        {
+            for(int i=1; i<=10; i++)
+            {
+                if( i == get_floor() )
+                {
+                    cout << "[" << i << "] " ;
+                    continue;
+                }
+                cout << i << " ";
+            }
+            cout << endl;
+        } 
         void move(int current, int floor) // move the elevator to the destination and show every floor it visited
         {
             if(current > floor) // if the elevator need to go down
             {
-                cout << "elevator down" << endl;
+                //cout << "elevator down" << endl; //*************************
                 while(get_floor() > floor) 
                 {
-                    display_floor();
+                    //display_floor(); //******************************
                     set_floor(get_floor()-1);
                 }
 
-                display_floor(); // display the final floor
-                cout << "elevator arrived" << endl;
+                //display_floor(); // display the final floor //**************************
+                //cout << "elevator arrived" << endl; //******************************
             }
             else // if the elevator need to go up
             {
-                cout << "elevator up" << endl;
+                //cout << "elevator up" << endl;
                 while(get_floor() < floor)
                 {
-                    display_floor();
+                    //display_floor(); //*********************************
                     set_floor(get_floor()+1);
                 }
 
-                display_floor(); // display the final floor
-                cout << "elevator arrived" << endl;
+                //display_floor(); // display the final floor //*****************************
+                //cout << "elevator arrived" << endl; //**********************************
             }
         }
 };
@@ -50,8 +62,20 @@ int main()
 
     int current, floor;
 
+    for(int i=1; i<=2; i++)
+    {
+        cout << "elevator " << i << ": " << "[" << i << "]";
+        for(int j=2; j<=10; j++)
+        {
+            cout << j  << " ";
+        }
+        cout << endl;
+    }
+        
+
     for(int i=0; true; i++)
     {
+        cout << endl << "Please input the current floor and the destination:" << endl;
         cin >> current >> floor;  // user input
 
         if( current<1 || current>10 || floor<1 || current>10 ) // invalid input
@@ -65,12 +89,20 @@ int main()
             cout << "call elevator1" << endl;
             elevator1.move(elevator1.get_floor(), current); //move the elevator to the current floor
             elevator1.move(elevator1.get_floor(), floor); // move the elevator to the destination floor
+            cout << "elevator 1: " ;
+            elevator1.display_floor();
+            cout << "elevator 2: ";
+            elevator2.display_floor();
         }
         else //elevator2 is closer
         {
             cout << "call elevator2" << endl;
             elevator2.move(elevator2.get_floor(), current); //move the elevator to the current floor
             elevator2.move(elevator2.get_floor(), floor); // move the elevator to the destination floor
+            cout << "elevator 1: " ;
+            elevator1.display_floor();
+            cout << "elevator 2: ";
+            elevator2.display_floor();
         }
 
     }
